06dffab1f0d35ee85f105d40f38543bc4aeaf81b
primemix <monstrpro@gmail.com>
2017-10-04 13:35:50 +0300

    Add app.php(config Route) and modified front.php

--- src/pages/index.php
+++ src/pages/index.php
@@ -1,2 +1,2 @@
 <!-- example.com/src/pages/index.php -->
-Hello <?php echo htmlspecialchars(isset($name) ? $name : 'GeekHub', ENT_QUOTES, 'UTF-8') ?>
+Hello <?php echo htmlspecialchars($name, ENT_QUOTES, 'UTF-8') ?>
